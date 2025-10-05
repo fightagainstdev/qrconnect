5039ea2d48d3892908f2c92ffd182d5a9a4b45f1
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-10-05 18:12:00 +0800

    Initial commit

---
+++ frontend/src/components/NoFriendsFound.jsx
@@ -0,0 +1,12 @@
+const NoFriendsFound = () => {
+  return (
+    <div className="card bg-base-200 p-6 text-center">
+      <h3 className="font-semibold text-lg mb-2">还没有朋友</h3>
+      <p className="text-base-content opacity-70">
+        与下面的语言伙伴连接，开始一起练习！
+      </p>
+    </div>
+  );
+};
+
+export default NoFriendsFound;
